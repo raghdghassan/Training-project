14c86b02762a70d6a7feeb9c7a7310237ddb54a8
raghdghassan <raghdghassan@gmail.com>
2017-08-08 11:42:16 +0300

    8/8

--- access-lists/src/test/java/Testing/TelnetTest.java
+++ access-lists/src/test/java/Testing/TelnetTest.java
@@ -10,24 +10,23 @@ import Connection.Telnet;
 import junit.framework.TestCase;
 
 public class TelnetTest extends TestCase {
-	Telnet telnet ; 
-	
-public static String server ; 
-public static int port ; 
-	   
-	   
-// assigning the values
-@Before
-	   protected void setUp() {
-		   server = "10.63.10.206";
-		   port= 23;
-	
-	   }
-	  @Test
-	   public void testTelnet() throws UnknownHostException, IOException{
-		   telnet = new Telnet(server,port) ; 
-	     
-	   }
+	Telnet telnet;
 
+	public static String server;
+	public static int port;
+
+	// assigning the values
+	@Before
+	protected void setUp() {
+		server = "10.63.10.206";
+		port = 23;
+
+	}
+
+	@Test
+	public void testTelnet() throws UnknownHostException, IOException {
+		telnet = new Telnet(server, port);
+
+	}
 
 }
